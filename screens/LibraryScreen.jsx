ffb3d517352d8f5a60aac6e977d27794c10a53b3
jabedzaman <jabedzaman2004@gmail.com>
2023-02-26 04:56:43 +0530

    create library screen

--- screens/LibraryScreen.jsx
+++ screens/LibraryScreen.jsx
@@ -1,17 +1,71 @@
-import { useColorScheme, Text } from "react-native";
+import { useColorScheme, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts, Comfortaa_600SemiBold } from "@expo-google-fonts/comfortaa";
+import Lottie from "lottie-react-native";
 
 const LibraryScreen = () => {
   const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Comfortaa_600SemiBold,
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView
       style={{
         backgroundColor: colorScheme === "dark" ? "#171717" : "#fff",
         height: "100%",
+        paddingHorizontal: 10,
       }}
     >
-      <Text>LibraryScreen</Text>
+      <Text
+        style={{
+          fontFamily: "Comfortaa_600SemiBold",
+          fontSize: 28,
+          color: colorScheme === "dark" ? "#fff" : "#000",
+        }}
+      >
+        Library
+      </Text>
+      <ScrollView
+        style={{
+          marginTop: 20,
+        }}
+      >
+        {/* <Text
+          style={{
+            fontFamily: "Comfortaa_600SemiBold",
+            fontSize: 20,
+            color: colorScheme === "dark" ? "#fff" : "#000",
+          }}
+        >
+          Recently watched
+        </Text> */}
+
+        <Lottie
+          source={require("../assets/animations/cat.json")}
+          autoPlay
+          loop
+          style={{
+            width: "100%",
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "Comfortaa_600SemiBold",
+            fontSize: 16,
+            color: colorScheme === "dark" ? "#fff" : "#000",
+            marginTop: 40,
+            alignSelf: "center",
+          }}
+        >
+          Nothing here yet.
+        </Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
